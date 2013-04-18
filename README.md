41acf6ec91fc4b368e53a3c50f1b17657cfd23bf
Marina Glancy <marina@moodle.com>
2013-04-18 16:43:17 +1000

    Tool for screenshots and changing themes in Behat

---
+++ README.md
@@ -0,0 +1,42 @@
+tool_behatui introduces features for changing the current theme and saving screenshots.
+The main objective is to be able to see how particular screen looks in all/several themes.
+
+More about Acceptance testing: http://docs.moodle.org/dev/Acceptance_testing
+
+First you need to define $CFG->behat_screenshots_dir in config.php
+Screenshots for each test run will be stored in separate subdirectories.
+
+Second, you can create .feature file. Example of scenario:
+
+    Given I log in as "admin"
+    When I am on homepage
+    Then Save a screenshot
+    When I change frontpage display to Combo list
+    And I change guest frontpage display to List of categories,Course search box
+    And Repeat in themes "Afterburner,Anomaly,Arialist,Binarius,Boxxie,Brick,Formal white,FormFactor,Fusion,Leatherbound,Magazine,Nimble,Nonzero,Overlay,Serenity,Simple,Sky High,Splash,Standard (legacy),Standard":
+      """
+      When I am on homepage
+      Then Save a screenshot as homepage_admin_{themename}
+      And I log out
+      And Save a screenshot as homepage_guest_{themename}
+      And I log in as "admin"
+      """
+    And I change theme to MyMobile
+    Then Save a screenshot as homepage_admin_{themename}
+    And I log out
+    And Save a screenshot as homepage_guest_{themename}
+
+Please note:
+You must be logged in as admin in order to change theme, repeat in themes and
+change frontpage display. You don't have to be admin to take a screenshot.
+
+
+
+Known bugs
+----------
+
+You need to have pretty wide browser window to change theme from Arialist to anything else.
+It works if you resize the browser window in the beginning of the test.
+
+You can not change the theme from MyMobile to anything else (therefore MyMobile test is
+located in the end of example)
---
+++ tests/behat/behat_ui.php
@@ -0,0 +1,218 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Making screenshots and other functions for testing course listings UI
+ *
+ * @package   core
+ * @category  test
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../../../lib/behat/behat_base.php');
+
+use Behat\Behat\Context\Step\Given as Given,
+        Behat\Mink\Exception\ElementNotFoundException as ElementNotFoundException,
+        Behat\Gherkin\Node\PyStringNode as PyStringNode;
+
+/**
+ * Making screenshots and other functions for testing course listings UI
+ *
+ * $CFG->behat_screenshots_dir must be specified in config.php
+ *
+ * @package   core
+ * @category  test
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_ui extends behat_base {
+    static $lasttheme = 'Standard';
+
+    /**
+     * Saves screenshot with default filename
+     *
+     * @Then /^Save a screenshot$/
+     * @param string $filename
+     */
+    public function save_a_screenshot() {
+        $this->save_a_screenshot_as(null);
+    }
+
+    /**
+     * Saves screenshot with specified filename
+     *
+     * @Then /^Save a screenshot as (.*)$/
+     * @param string $filename
+     */
+    public function save_a_screenshot_as($filename) {
+        global $CFG;
+        if (empty($CFG->behat_screenshots_dir)) {
+            // no error, just skip making screenshots
+            return true;
+        }
+
+        // get filepath unique for current test run
+        static $filepath = null;
+        if ($filepath === null) {
+            if (!is_writable($CFG->behat_screenshots_dir)) {
+                throw coding_exception('$CFG->behat_screenshots_dir is not writtable');
+            }
+            $path = date('Y-m-d_H:i') . '_';
+            for ($cnt = 0; $cnt < 10000; $cnt++) {
+                $filepath = $CFG->behat_screenshots_dir . DIRECTORY_SEPARATOR . $path. sprintf('%04d', $cnt);
+                if (!file_exists($filepath)) {
+                    break;
+                }
+            }
+            mkdir($filepath);
+        }
+
+        $filename = preg_replace('/\{themename\}/', self::$lasttheme, $filename);
+        // find unused file name
+        $filename = trim($filename);
+        if (!empty($filename) && !preg_match('/\.png$/i', $filename)) {
+            $filename .= '.png';
+        }
+        if (empty($filename) || file_exists($filepath.DIRECTORY_SEPARATOR.$filename)) {
+            if (empty($filename)) {
+                // default filename should always have _0000 postfix
+                $filename = 'screenshot.png';
+            }
+            for ($cnt = 0; $cnt < 10000; $cnt++) {
+                $newfilename = preg_replace('/(\.png)$/i', '_'.  sprintf('%03d', $cnt). '$1', $filename);
+                if (!file_exists($filepath. DIRECTORY_SEPARATOR. $newfilename)) {
+                    break;
+                }
+            }
+            $filename = $newfilename;
+        }
+
+        // save screenshot
+        $this->saveScreenshot($filename, $filepath);
+        return true;
+    }
+
+    /**
+     * Changes the site theme (default device) and proceeds to homepage
+     *
+     * @When /^I change theme to (.*)$/
+     * @param string $themename
+     */
+    public function i_change_theme_to($themename) {
+        if (self::$lasttheme === $themename) {
+            return true;
+        }
+        self::$lasttheme = $themename;
+        return array(
+            new Given('I am on homepage'),
+            new Given('I expand "Site administration" node'),
+            new Given('I expand "Appearance" node'),
+            new Given('I expand "Themes" node'),
+            new Given('I follow "Theme selector"'),
+            new Given('I press "Change theme"'),
+            new Given('I click on "Use theme" "button" in the "'.$themename.'" table row'),
+            new Given('I am on homepage')
+        );
+    }
+
+    /**
+     * @Then /^Repeat in themes "([^"]*)":$/
+     */
+    public function repeat_in_themes($themes, PyStringNode $commands)
+    {
+        $rv = array();
+        foreach (preg_split('/\s*,\s*/', $themes, 0, PREG_SPLIT_NO_EMPTY) as $theme) {
+            $rv[] = new Given("I change theme to $theme");
+            foreach (preg_split('/\s*\n\s*/', $commands->getRaw(), 0, PREG_SPLIT_NO_EMPTY) as $command) {
+                $command = preg_replace(array('/^When /','/^Given /','/^Then /','/^And /'), '', $command);
+                $rv[] = new Given($command);
+            }
+        }
+        return $rv;
+    }
+
+    /**
+     * Waits with the provided params if we are running a JS session.
+     *
+     * @param int $timeout
+     * @param string $javascript
+     * @return void
+     */
+    protected function wait($timeout, $javascript = false) {
+        if ($this->running_javascript()) {
+            $this->getSession()->wait($timeout, $javascript);
+        }
+    }
+
+    /**
+     * Changes the $CFG->frontpageloggedin
+     *
+     * @When /^I change frontpage display to (.*)$/
+     * @param string $value
+     */
+    public function i_change_frontpage_display_to($value) {
+        $this->change_admin_multiselect('frontpageloggedin', $value);
+    }
+
+    /**
+     * Changes the $CFG->frontpage
+     *
+     * @When /^I change guest frontpage display to (.*)$/
+     * @param string $value comma-separated list of frontpage sections
+     */
+    public function i_change_guest_frontpage_display_to($value) {
+        $this->change_admin_multiselect('frontpage', $value);
+    }
+
+    /**
+     * 
+     * @param string $label either 'frontpage' or 'frontpageloggedin'
+     * @param string $value comma-separated list of frontpage sections
+     */
+    private function change_admin_multiselect($label, $value) {
+        // Unfortunately we can't use behat_admin::i_set_the_following_administration_settings_values() here because the structure is different
+
+        // We expect admin block to be visible, otherwise go to homepage.
+        if (!$this->getSession()->getPage()->find('css', '.block_settings')) {
+            $this->getSession()->visit($this->locate_path('/'));
+            $this->wait(self::TIMEOUT, '(document.readyState === "complete")');
+        }
+
+        // Search by label.
+        $searchbox = $this->find_field('Search in settings');
+        $searchbox->setValue($label);
+        $submitsearch = $this->find('css', 'form.adminsearchform input[type=submit]');
+        $submitsearch->press();
+
+        $this->wait(self::TIMEOUT, '(document.readyState === "complete")');
+
+        // Admin settings does not use the same DOM structure than other moodle forms
+        // but we also need to use lib/behat/form_field/* to deal with the different moodle form elements.
+        $exception = new ElementNotFoundException($this->getSession(), '"' . $label . '" administration setting ');
+
+        $values = preg_split('/\s*,\s*/', $value, 0, PREG_SPLIT_NO_EMPTY);
+        $fieldnodes = $this->find_all('css', "#admin-$label select", $exception);
+        $i = 0;
+        foreach ($fieldnodes as $fieldnode) {
+            $field = behat_field_manager::get_field_instance('select', $fieldnode, $this->getSession());
+            $field->set_value(isset($values[$i]) ? $values[$i] : 'None');
+            $i++;
+        }
+
+        $this->find_button('Save changes')->press();
+    }
+}
